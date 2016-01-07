27ab4fd69a5eee288f7531049dbc23674754fdf6
Alex Espinoza <aespinoza@aspria.net>
2016-01-07 16:09:58 -0500

    Finish up BandcampParser, correctly retrieving data from import.io

--- app/models/bandcamp_parser.rb
+++ app/models/bandcamp_parser.rb
@@ -1,12 +1,20 @@
 # BandcampParser gets newest albums from a bandcamp artist/label page based on the source's url (which should be a bandcamp url)
+# Currently uses import.io to parse the web page and HTTParty to query the import.io API to get the page's content in JSON format
+# http://api.docs.import.io/
 
 class BandcampParser
+  include HTTParty
+  base_uri "https://api.import.io"
+
   def initialize(source)
     @source = source
     @source_url = source.url
+    @import_io_api_key = Rails.application.secrets.import_io_api_key
   end
 
   def get_newest_entries
-    puts @source_url
+    response = self.class.get("/store/connector/_magic", query: {url: @source_url, _apikey: @import_io_api_key})
+    newest_entries = response["tables"][0]["results"]
+    newest_entries
   end
 end
--- lib/tasks/sources.rake
+++ lib/tasks/sources.rake
@@ -2,10 +2,13 @@ namespace :sources do
   desc "Get new entries from each source"
   task fetch_new_entries: :environment do
   	sources = Source.all
+    
     sources.each do |source|
+      latest_entry = source.entries.last
       source_identifier = SourceIdentifier.new(source)
       parser = source_identifier.identify_parser
       newest_entries = parser.get_newest_entries
+      #entry_saver = EntrySaver.new(newest_entries, latest_entry)
     end
   end
 end
