27c0dd65e2ded3ca5482ce2f2a449127982e0dba
Alex Espinoza <aespinoza@aspria.net>
2016-02-03 23:38:52 -0500

    Update seeds.rb for soundcloud testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 Source.create(name: "Orange Milk Records", parser_format: "bandcamp", url: "http://orangemilkrecords.bandcamp.com")
+Source.create(name: "Cascine", parser_format: "soundcloud", soundcloud_id: "cascine")
